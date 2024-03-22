af58b0ccbf8808253cbaa91a8bc60c3f5bdd56e1
albinjohn <128218167+furiousAJ@users.noreply.github.com>
2024-03-23 03:08:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import axios from 'axios';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Flexslide from './Components/Userside/Userhome/Flexslide';
+import Home from './Components/Sellerside/Sellerhome/Home';
+import Usersignup from './Components/Userside/Userlogin/Usersignup';
+import Footer from './Components/Userside/Userfooter/Footer';
+import Midpart from './Components/Userside/Userfooter/Midpart';
+import Cart from './Components/Userside/Userhome/Cart';
+import CategoryFilter from './Components/Userside/Userfilter/CategoryFilter';
+import CategoryPage from './Components/Userside/Userfilter/CategoryPage';
+import Sellerlogin from './Components/Sellerside/Sellerlogin/Sellerlogin';
+import Sellersignup from './Components/Sellerside/Sellerlogin/Sellersignup';
+import Login from './Components/Userside/Userlogin/Login';
+import Addproduct from './Components/Sellerside/Selleradd/Addproduct';
+import Recentlyadd from './Components/Sellerside/Selleradd/Rcentlyadd';
+import Productlist from './Components/Sellerside/Selleradd/Productlist';
+import Adminlogin from './Components/Adminside/Adminlogin/Adminlogin';
+
+import Adminviewproduct from './Components/Adminside/Adminadd/Adminviewproduct';
+import Addcategory from './Components/Adminside/Adminadd/Addcategory';
+
+import CategoryView from './Components/Adminside/Adminadd/Categoryview';
+import Productview from './Components/Sellerside/Selleradd/Productview';
+
+import Order from './Components/Userside/Userhome/Order';
+import Aboutus from './Components/Userside/Userhome/Aboutus';
+import Categoryedit from './Components/Adminside/Adminadd/Categoryedit';
+import Incomingorder from './Components/Sellerside/Selleradd/Incomingorder';
+import Profile from './Components/Sellerside/Sellerprofile/Profile';
+import Orderview from './Components/Userside/Userhome/Orderview';
+import Usersentry from './Components/Adminside/Adminadd/Usersentry';
+import Sellerentry from './Components/Adminside/Adminadd/Sellerentry';
+import Userprofile from './Components/Userside/Userhome/Userprofile';
+import Userprofileedit from './Components/Userside/Userhome/Userprofileedit';
+import SearchResults from './Components/Userside/Userhome/SearchResults';
+
+function App() {
+  const sellerId = localStorage.getItem('sellerId');
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {/* Userside */}
+          <Route path='/signup' element={<Usersignup />} />
+          <Route path='/' element={<Login />} />
+          <Route path='/userside/:id' element={<Flexslide /> } />
+          <Route path="/search-results" element={<SearchResults/>} />
+          <Route path='/footer' element={<Footer />} />
+          <Route path='/mid' element={<Midpart />} />
+          <Route path='/cart' element={ <Cart /> } />
+           <Route path='/oview' element={<Order /> } />
+           <Route path='/about' element={<Aboutus />  }/>
+         <Route path='/filter' element={<CategoryFilter />} />
+          <Route path='/category/:category' element={ <CategoryPage />} />
+          <Route path='/order' element={ <Orderview /> } />
+
+          <Route path='/userprofile' element={<Userprofile method='get' />} />
+          <Route path='/uedit' element={ <Userprofileedit /> } />
+          
+
+          
+          {/* Sellerside */}
+          <Route path='/seller/:id' element={<Home />} />
+          <Route path='/sellersignup' element={<Sellersignup />} />
+          <Route path='/sellerlogin' element={<Sellerlogin />} />
+          <Route path='/add' element={<Addproduct method='post' />} />
+          <Route path='/pview/:id' element={<Productview method='get' sellerId={sellerId} />} />
+          <Route path='/productlist' element={<Productlist method='get'/>} />
+          <Route path='/products' element={<Recentlyadd />} />
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path='/sellerorder' element={<Incomingorder />} />
+
+          {/* Admin */}
+          <Route path='/alogin' element={<Adminlogin />} />
+          
+          <Route path='/category'element={<Addcategory method='post'/>} />
+          <Route path='/viewcategory' element={<CategoryView method='get' />} />
+          <Route path='/adminallproduct' element={<Adminviewproduct />} />   
+          <Route path='/userlog' element={<Usersentry />} />  
+          <Route path='/sellerlog' element={<Sellerentry />} /> 
+          <Route path='/cedit' element={<Categoryedit />} />     
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ productnew.js
@@ -0,0 +1,221 @@
+const app = require('express').Router();
+const { request, response } = require('express');
+const ProductModel = require("../model/product");
+const multer = require('multer');
+const mongoose = require('mongoose');
+
+
+const storage = multer.memoryStorage(); // Store images in memory
+const upload = multer({ storage: storage });
+
+
+app.post('/productnew', upload.single('Photo'), async (req, res) => {
+    try {
+        const {
+            sellerId,
+            Productname,
+            Productprice,
+            Quantity,
+            Cid,
+            Status,
+            Description
+        } = req.body;
+
+        console.log('data',req.body);
+
+      
+        
+        // Check if SellerId is provided
+        if (!sellerId) {
+            return res.status(400).json({ error: 'SellerId is required' });
+        }
+
+        console.log("sellerId:", sellerId); // Print the seller's ID in the console
+
+        const newProduct = new ProductModel({
+            sellerId, // Assigning the seller ID to the product
+            Productname,
+            Productprice,
+            Quantity,
+            Cid,
+            Status,
+            Description,
+            Photo: {
+                data: req.file.buffer,
+                contentType: req.file.mimetype,
+            }
+        });
+
+        await newProduct.save();
+        res.status(200).json({ message: 'Product added successfully' });
+        console.log(newProduct);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+
+
+    app.put('/delete/:id',async(request,response)=>{
+        let id = request.params.id
+        await ProductModel.findByIdAndUpdate(id,{$set:{Status:"Inactive"}})
+        response.send("Record Deleted")
+    })
+
+    app.put('/active/:id',async(request,response)=>{
+        let id = request.params.id
+        await ProductModel.findByIdAndUpdate(id,{$set:{Status:"Active"}})
+        response.send("Record Active")
+    })
+
+    
+    app.get('/productview', async (req, res) => {
+        try {
+            const sellerId = req.query.sellerId;
+            if (!sellerId) {
+                return res.status(400).json({ message: 'Seller ID is required' });
+            }
+    
+            const products = await ProductModel.find({ sellerId: sellerId }).populate('Cid').populate('Photo');
+            if (products.length === 0) {
+                return res.status(404).json({ message: 'No products found for this seller' });
+            }
+    
+            res.status(200).json(products);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+
+
+    
+    
+
+    
+    // app.put('/editproduct/:id',async(request,response)=>{
+    //     let id = request.params.id
+    //     await ProductModel.findByIdAndUpdate(id,request.body)
+    //     response.send("Record updated")
+    // })
+    app.put('/editproduct/:id', upload.single('Photo'), async (request, response) => {
+        try {
+          const id = request.params.id;
+          const { Productname, Productprice, Quantity, Cid, Status, Description } = request.body;
+          let result = null;
+      
+          // Check if Cid is a valid ObjectId
+          if (!mongoose.Types.ObjectId.isValid(Cid)) {
+            return response.status(400).json({ message: 'Invalid Cid provided' });
+          }
+      
+          if (request.file) {
+            const updatedData = {
+              Productname, 
+              Productprice,
+              Quantity,
+              Cid: mongoose.Types.ObjectId(Cid), // Convert Cid to ObjectId
+              Status,
+              Description,
+              Photo: {
+                data: request.file.buffer,
+                contentType: request.file.mimetype,
+              }
+            };
+            result = await ProductModel.findByIdAndUpdate(id, updatedData);
+          } else {
+            const updatedData = {
+              Productname, 
+              Productprice,
+              Quantity,
+              Cid: mongoose.Types.ObjectId(Cid), // Convert Cid to ObjectId
+              Status,
+              Description
+            };
+            result = await ProductModel.findByIdAndUpdate(id, updatedData);
+          }
+      
+          if (!result) {
+            return response.status(404).json({ message: 'Item not found' });
+          }
+      
+          response.status(200).json({ message: 'Item updated successfully', data: result });
+        } catch (error) {
+          console.error(error);
+          response.status(500).json({ error: 'Internal Server Error' });
+        }
+      });
+      
+    app.get('/adminproductview', async (request, response) => {
+        try {
+            const result = await ProductModel.aggregate([
+                {
+                    $lookup: {
+                        from: 'categories',
+                        localField: 'Cid',
+                        foreignField: '_id',
+                        as: 'category',
+                    },
+                },
+                {
+                    $lookup: {
+                        from: 'sellers',
+                        localField: 'seller',
+                        foreignField: '_id',
+                        as: 'seller',
+                    },
+                },
+            ]);
+    
+            // Log the result to the console
+        ;
+    
+            response.send(result);
+        } catch (error) {
+            console.error(error);
+            response.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+
+
+// Endpoint for searching products
+app.get('/search', async (req, res) => {
+    try {
+        const searchQuery = req.query.query; // Retrieve the search query from request query parameters
+        if (!searchQuery) {
+            return res.status(400).json({ message: 'Search query is required' });
+        }
+
+        // Perform case-insensitive search for products with Productname containing the search query
+        const products = await ProductModel.find({ Productname: { $regex: new RegExp(searchQuery, 'i') } });
+
+        if (products.length === 0) {
+            return res.status(404).json({ message: 'No products found matching the search query' });
+        }
+
+        res.status(200).json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+    app.get('/userallproduct',async(request,response)=>{
+        const result = await ProductModel.aggregate([
+            {
+              $lookup: {
+                from: 'categories', // Name of the other collection
+                localField: 'Cid', // field of item
+                foreignField: '_id', //field of category
+                as: 'prod',
+              },
+            },
+          ]);
+        
+          response.send(result)
+        })
+    
+    
+module.exports = app;
